f2aececff1967b964658b6c0dc248dec61f4e613
Haley Bowes <bramsonbowes.haley@gmail.com>
2018-05-23 09:25:20 -0400

    adding to github

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,10 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor() {
+      super();
+    }
+
+
+    handleClick(e) {
+      e.preventDefault();
+      function success(position) {
+        var latitude = position.coords.latitude;
+        console.log(latitude);
+        var longitude = position.coords.longitude;
+        console.log(longitude);
+      }
+
+      navigator.geolocation.getCurrentPosition(success);
+    }
+
+   
+        
+      
+        
+    
+
+
     render() {
       return (
         <div>
-          Hello
+          <button onClick={this.handleClick}>Show my location</button>
+          <div id="out"></div>
         </div>
       )
     }

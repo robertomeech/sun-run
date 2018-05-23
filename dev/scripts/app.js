e453524c6643e3702f1009c6b89386f8f0c7e8bb
Haley Bowes <bramsonbowes.haley@gmail.com>
2018-05-23 17:40:02 -0400

    added the date picker

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,39 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// const DatePicker = require('react-date-picker');
+import DatePicker from 'react-date-picker/dist/entry.nostyle';
+
 
 class App extends React.Component {
     constructor() {
       super();
       this.state = {
-        latitude: '',
-        longitude: ''
+        date: new Date()
       }
-      this.handleClick = this.handleClick.bind(this)
     }
 
+    // onChange = date => this.setState({ date })
 
-    handleClick(e) {
-      e.preventDefault();
+    onChange(date) {
+      this.setState({
+        date: Date(date)
+      })
+    }
 
-      function success(position) {
+    componentDidMount() {
+       function success(position) {
         let latitude = position.coords.latitude;
         console.log(latitude)
         let longitude = position.coords.longitude;
         console.log(longitude)
+
+
+
+        
       }
 
       navigator.geolocation.getCurrentPosition(success);
+    }
 
-      }
      
     render() {
       return (
         <div>
-          <button onClick={this.handleClick}>Show my location</button>
-          <div id="out"></div>
+          {/* <button onClick={this.handleClick}>Show my location</button> */}
+          <div id="out">Testing</div>
+          <DatePicker
+            onChange={this.onChange}
+            value={this.state.date}
+          />
         </div>
       )
     }
 }
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('app'));

fed34cc6808fbd5245078f476ba4024455b0e5ed
Christian Fowler <christianhfowler@gmail.com>
2018-05-23 15:53:32 -0400

    pulling haleys changes

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 class App extends React.Component {
     render() {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log(position.coords.latitude, position.coords.longitude);
+        });
       return (
         <div>
           Hello

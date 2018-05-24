b29c3b844843421e6f65e23cd762de32ddc3ba9f
Haley Bowes <bramsonbowes.haley@gmail.com>
2018-05-23 20:36:44 -0400

    updated functions

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,41 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// const DatePicker = require('react-date-picker');
 import DatePicker from 'react-date-picker/dist/entry.nostyle';
+import axios from 'axios';
 
 
 class App extends React.Component {
     constructor() {
       super();
       this.state = {
-        date: new Date()
+        date: new Date(),
+        latitude:'',
+        longitude:''
       }
+    this.onChange = this.onChange.bind(this)
+    this.success = this.success.bind(this)
+    this.getAxios = this.getAxios.bind(this)
+    // this.axiosCall = this.axiosCall.bind(this)
     }
 
-    // onChange = date => this.setState({ date })
-
-    onChange(date) {
+    onChange(dateClicked) {
       this.setState({
-        date: Date(date)
+        date: dateClicked
       })
+
+      this.getAxios()
+      // navigator.geolocation.getCurrentPosition(this.success);
+    }
+
+    getAxios(){
+      axios.get('https://api.sunrise-sunset.org/json', {
+        params: {
+          lat: this.state.latitude,
+          lng: this.state.longitude,
+          date: this.state.date
+        }
+      })
+        .then((res) => {
+          console.log(res.data)
+        })
+    }
+
+    success(position) {
+      let latitude = position.coords.latitude;
+      console.log(latitude)
+      let longitude = position.coords.longitude;
+      console.log(longitude)
+      this.setState({
+        latitude: latitude,
+        longitude: longitude
+      })
+
+      this.getAxios();
     }
 
     componentDidMount() {
-       function success(position) {
-        let latitude = position.coords.latitude;
-        console.log(latitude)
-        let longitude = position.coords.longitude;
-        console.log(longitude)
-
-
-
-        
-      }
-
-      navigator.geolocation.getCurrentPosition(success);
+      navigator.geolocation.getCurrentPosition(this.success);
+      console.log('hello???');
+      // this.getAxios()
     }
 
-     
     render() {
       return (
         <div>

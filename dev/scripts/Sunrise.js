ce68f2f15b2e3088c087d244fad088b4dc6ef40b
Christian Fowler <christianhfowler@gmail.com>
2018-05-25 12:42:56 -0400

    Ryan saves the day

--- dev/scripts/Sunrise.js
+++ dev/scripts/Sunrise.js
@@ -3,63 +3,10 @@ import React from 'react';
 import axios from 'axios';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow, DirectionsRenderer} from 'react-google-maps';
 import{compose, withProps, lifecycle} from 'recompose';
-
-const MapWithADirectionsRenderer = compose(
-    withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
-        loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `400px` }} />,
-        mapElement: <div style={{ height: `100%` }} />,
-    }),
-    withScriptjs,
-    withGoogleMap,
-    lifecycle({
-        componentDidMount() {
-            const DirectionsService = new google.maps.DirectionsService();
-
-            DirectionsService.route({
-                origin: new google.maps.LatLng(this.props.lat, this.props.lng),
-                destination: new google.maps.LatLng(this.props.lat, this.props.lng),
-                travelMode: google.maps.TravelMode.WALKING,
-            }, (result, status) => {
-                if (status === google.maps.DirectionsStatus.OK) {
-                    this.setState({
-                        directions: result,
-                    });
-                    // console.log(result)
-                } else {
-                    console.error(`error fetching directions ${result}`);
-                }
-            });
-        }
-    })
-)(props =>
-    <GoogleMap
-        defaultZoom={7}
-        defaultCenter={new google.maps.LatLng(41.8507300, -87.6512600)}
-    >
-            {props.directions && <DirectionsRenderer directions={props.directions} />}
-    </GoogleMap>
-);
+import MapWithADirectionsRenderer from './Map.js'
 
 
-// const MyMapComponent = compose(
-//     withProps({
-//         googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDXgVNpWjqGce14Uv4RKfgk4cVaSSSC7Hg&v=3.exp&libraries=geometry,drawing,places",
-//         loadingElement: <div style={{ height: `100%` }} />,
-//         containerElement: <div style={{ height: `400px` }} />,
-//         mapElement: <div style={{ height: `100%` }} />,
-//     }),
-//     withScriptjs,
-//     withGoogleMap
-// )((props) =>
-//     <GoogleMap
-//         defaultZoom={1}
-//         defaultCenter={{ lat: -34.397, lng: 150.644 }}
-//     >
-//         {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-//     </GoogleMap>
-// )
+
 
 
     
@@ -69,12 +16,33 @@ class Sunrise extends React.Component {
     constructor(){
         super();
         this.handleClick = this.handleClick.bind(this)
+        this.state={
+            destination: "",
+            finalDestination: "Toronto"
+            
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
         // this.getLandmark = this.getLandmark.bind(this)
     }
 
    
        
-    
+    handleChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+        // console.log(e.target.value)
+    }
+    handleSubmit(e){
+        e.preventDefault();
+
+        const destClone = this.state.destination
+        this.setState({
+            finalDestination: destClone
+        })
+
+    }
 
 
     handleClick(){
@@ -82,18 +50,18 @@ class Sunrise extends React.Component {
     }
     render() {
        
-
         
-
+        console.log(this.state.finalDestination)
         return(
             <div>
                 <p>The sunrise will be at {this.props.sunriseTime}</p>
-                <form action="#">
-                    <input type="text" placeholder="enter a landmark?"/>
+                <form action="#" onSubmit={this.handleSubmit}>
+                    <input type="text" name="destination" onChange={this.handleChange} value={this.state.destination}placeholder="enter a landmark?"/>
+                    <input type="submit" name="submit" />
                 </form>
                 <div id="map"></div>
-                <button onClick={this.handleClick}>content</button>
-                <MapWithADirectionsRenderer lat={this.props.lat} lng={this.props.long} />
+                
+                <MapWithADirectionsRenderer lat={this.props.lat} lng={this.props.long} destination={this.state.finalDestination}/>
 
                 
                 

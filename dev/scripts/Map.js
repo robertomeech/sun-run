8130bbe75996e19978338274200ac939f56c4d92
Haley Bowes <bramsonbowes.haley@gmail.com>
2018-05-28 11:07:13 -0400

    updating getting prop info

--- dev/scripts/Map.js
+++ dev/scripts/Map.js
@@ -3,8 +3,7 @@ import React from 'react';
 import axios from 'axios';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow, DirectionsRenderer } from 'react-google-maps';
 import { compose, withProps, lifecycle, withState} from 'recompose';
-let duration = '';
-let distance = '';
+
 const MapWithADirectionsRenderer = compose(
     withProps({
         googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
@@ -20,8 +19,6 @@ const MapWithADirectionsRenderer = compose(
         componentWillReceiveProps(nextProps){
             if(this.props.destination !== nextProps.destination){
                 const DirectionsService = new google.maps.DirectionsService();
-    
-            
                 DirectionsService.route({
                     origin: new google.maps.LatLng(this.props.lat, this.props.lng),
                     destination: nextProps.destination,
@@ -34,16 +31,9 @@ const MapWithADirectionsRenderer = compose(
                     }
                 });
             }
-            // console.log(nextProps.directions.routes[0].legs[0].distance.text)
-            // console.log(nextProps.directions.routes[0].legs[0].duration.text)
-            distance = nextProps.directions.routes[0].legs[0].distance.text
-            console.log(distance)
-            duration = nextProps.directions.routes[0].legs[0].duration.text
-            console.log(duration)
             // put time wizardry here
         },
         componentDidMount() {
-            // console.log(this)
             const DirectionsService = new google.maps.DirectionsService();
 
             DirectionsService.route({
@@ -60,29 +50,17 @@ const MapWithADirectionsRenderer = compose(
         }
     })
 )(props => {
-<<<<<<< HEAD
     console.log(props)
-    
-=======
-    // console.log(props.directions.request)
->>>>>>> 7dea9ac1621ff9b4a2e13f9dd41b00eeabab0139
-    // const one = (props.directions.routes[0])
-    // const two = (one.legs[0])
-    // const three = (two.duration.text)
-    // console.log(three)
-    // console.log(props.directions.routes[0].legs[0].duration.text)
     return(
         
         <div>
+            {props.directions && props.directions.routes[0].legs[0].distance.text}
             <GoogleMap
                 defaultZoom={7}
                 defaultCenter={new google.maps.LatLng(41.8507300, -87.6512600)}
             >
                 {props.directions && <DirectionsRenderer directions={props.directions} />}
             </GoogleMap>
-            <div>
-                <p className="testing">{duration}{distance}</p>
-            </div>
         </div>
     )
 }
